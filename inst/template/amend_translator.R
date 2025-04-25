230e9c6ac46db084241a6deda8ee72fde4f623b0
Marie MORFIN <mmorfin@IFREMER.FR>
2025-04-25 11:28:57 +0200

    Translator corrections
    What? % => %%

--- inst/template/amend_translator.R
+++ inst/template/amend_translator.R
@@ -7,16 +7,26 @@ translator <- read.csv(
 
 head(translator)
 
-translator[translator$id=="2_1_subtitle_frac_weight",]$EN<-"Catch weights by fraction (landings/discards)"
-translator[translator$id=="2_2_subtitle",]$FR<-"Comparaison des poids débarqués et rejetés par espèce"
+translator[translator$id=="2_2_discard_rate_STD",]$EN<-"The mean discarding rate by fishing operation is %i %% for the standard gear."
+translator[translator$id=="2_2_discard_rate_STD",]$FR<-"Le taux de rejet moyen par opération de pêche est de %i %% pour l'engin standard."
+
+
+translator[translator$id=="2_2_discard_rate_TEST",]$EN<-"The mean discarding rate by fishing operation is %i %% for the test gear."
+translator[translator$id=="2_2_discard_rate_TEST",]$FR<-"Le taux de rejet moyen par opération de pêche est de %i %% pour l'engin test."
+
+
+translator[translator$id=="2_1_sorting_var2",]$EN<-"A %s of %i %% of the sorting time is observed when considering the mean sorting time."
+translator[translator$id=="2_1_sorting_var2",]$FR<-"Une %s de %i %% de la durée de tri est observée avec l'engin test en considérant la durée moyenne."
+
+
 
 
 translator |> filter(id=="2_1_sorting_var1")
 
-new_tab<-data.frame(id="2_2_discard_rate_STD",
-                    description="Reporting sentence for the mean discarding rate by FO for the STD gear",
-                    EN="The mean discarding rate by fishing operation is %i % for the standard gear.",
-                    FR="Le taux de rejet moyen par opération de pêche est de %i % pour l'engin standard.")
+new_tab<-data.frame(id="2_2_discard_rate_TEST",
+                    description="Reporting sentence for the mean discarding rate by FO for the TEST gear",
+                    EN="The mean discarding rate by fishing operation is %i % for the test gear.",
+                    FR="Le taux de rejet moyen par opération de pêche est de %i % pour l'engin test.")
 
 
 translator<-rbind.data.frame(translator,new_tab)
